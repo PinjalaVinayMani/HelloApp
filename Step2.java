b47c8f3f656f645b8625cf9947d39d87624e2f49
PinjalaVinayMani <vinay2008pinjala@gmail.com>
2026-02-15 23:05:46 +0530

    Add files via upload

--- Step2.java
+++ Step2.java
@@ -1,5 +1,4 @@
 public class Step2 {
-
     public static void main(String[] args) {
         System.out.println("  ***       ***      ****      **** ");
         System.out.println(" *   *     *   *     *   *    *     ");
